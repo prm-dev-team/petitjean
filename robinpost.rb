8ed51a6dc8cf25a99336c90387b6f00e323ac31f
Eric Lacaille <eric.lacaille@gitguardian.com>
2021-01-25 14:48:45 +0100

    mon premier commit

--- robinpost.rb
+++ robinpost.rb
@@ -1,3 +1,8 @@
 class RobinPost
-	POSTMAN_KEY='PMAK-5e3c52895351d62230f49163-123cbbbd012271996d81dc087180e29d36'
+	POSTMAN3_KEY='PMAK-5e3c52895351d62230f49163-123cbbbd012271996d81dc087180e29d36'
+
+
+	def say_hello_celine
+		puts "say hello"
+	end
 end
